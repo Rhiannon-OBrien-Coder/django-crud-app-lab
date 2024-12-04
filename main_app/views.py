b2bf9b673208d94e5c2d845d36390cb4430e2909
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-12-04 15:03:16 -0600

    basic pages as templates with basic css, no database or images yet

---
+++ main_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+class Coffee:
+    def __init__(self, name, brand, description, rating):
+        self.name = name
+        self.brand = brand
+        self.description = description
+        self.rating = rating
+
+coffee = [
+    Coffee('Peppermint Mocha', 'Starbucks', 'A chocolate-y treat seasonally made with peppermint.', 2),
+    Coffee('Black Cat Classic Espresso', 'Cometeer Coffee', 'With notes of dark chocolate, raw sugar, and marshmallow.', 4),
+    Coffee('The Daily', 'Cometeer Coffee', 'With notes of chocolate, caramel, and molasses.', 5),
+    Coffee('Mocha Java', 'Cometeer Coffee', 'With notes of dark chocolate, almond, and berry.', 3)
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def coffee_index(request):
+    return render(request, 'coffee/index.html', {'coffee': coffee})
